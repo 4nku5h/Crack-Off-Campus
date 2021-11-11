d2c3f0425a7317e6b0e53ec3a27fa362b5266942
4nku5h <ankush9044045461@gmail.com>
2021-11-11 22:46:55 +0530

    three-animation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { setLoggedIn, setUserDetatil, setInformation_announcementDetails, setJob
 import store from './redux/store/store';
 //
 ///temp study links https://codepen.io/seanseansean/full/EaBZEY , https://www.youtube.com/watch?v=Vi3ij91wqaw
+//https://docs.google.com/document/d/1f547vOYxBIK1lE-DWu95TyCJWN32H4T0/edit?usp=sharing&ouid=113426037018131332881&rtpof=true&sd=true
+
 function App() {
     const isAccessWithoutLogin = useSelector(state => state.isAccessWithoutLogin);
     const isLoggedin = useSelector(state => state.isLoggedin)
@@ -22,8 +24,10 @@ function App() {
             ) :
                 (
                     <MainComponent />
+                    
                 )
         }
+        {console.log(App.displayName)}
         </div>
     )
 
--- src/Component/Loading/LoadingComponent.js
+++ src/Component/Loading/LoadingComponent.js
@@ -1,10 +1,72 @@
-import React, { useCallback } from "react";
+import React, { Suspense, useCallback, useMemo } from "react";
+
 //styles
 import './styles.css';
+
 //redux
 import { useDispatch, useSelector } from "react-redux";
-import { setAccessWithoutLogin, setLoginSignupComponentVisibility} from "../../redux/actions/actions";
+import { setAccessWithoutLogin, setLoginSignupComponentVisibility } from "../../redux/actions/actions";
 import LoginSignupComponent from "../LoginSignup/LoginSignupComponent";
+
+//Three
+import * as THREE from 'three';
+import { Canvas, useLoader } from 'react-three-fiber';
+import CircleImg from './../../assets/circle.png';
+
+function Points() {
+  const imgTex = useLoader(THREE.TextureLoader, CircleImg);
+  const count = 100;
+  const sep = 3;
+  let positions = useMemo(() => {
+    let positions = []
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = 0;
+        positions.push(x, y, z);
+      }
+    }
+    return new Float32Array(positions);
+  }, [count, sep] )
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attachObject={['attributes', 'position']}
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0x00AAFF}
+        size={0.5}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  )
+}
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}>
+      <Suspense fallback={null}>
+        <Points />
+      </Suspense>
+
+    </Canvas>
+  )
+
+}
 
 function LoadingComponent() {
   const isLoginSignupComponentVisible = useSelector(state => state.isLoginSignupComponentVisible);
@@ -16,12 +78,15 @@ function LoadingComponent() {
     }, 150)
   }
 
-
-  //loadMainComponent();
-
   return (
     <div className="Loading">
       <div className="Loading_inner">
+        <div className='anim'>
+          <Suspense fallback={<div>Loading</div>}>
+            <AnimationCanvas />
+          </Suspense>
+        </div>
+
         <h1 >Crack Off Campus</h1>
         <button id="btn_Continue" onClick={loadMainComponent_WithoutLogin}>Continue Without Login</button>
         <button id="btn_Login" onClick={() => { dispatch(setLoginSignupComponentVisibility(true)) }} >Login</button>
@@ -29,8 +94,8 @@ function LoadingComponent() {
           (
             <LoginSignupComponent />
           )
-          :null
-        }   
+          : null
+        }
       </div>
     </div>
   )
