21501fe7650906b4e848ba026e4fea42ca734085
4nku5h <ankush9044045461@gmail.com>
2021-11-28 17:58:28 +0530

    ui/ux

--- src/Component/CrackOffCampus-Home/PostComponent/PostCardComponent.js
+++ src/Component/CrackOffCampus-Home/PostComponent/PostCardComponent.js
@@ -7,18 +7,21 @@ function PostCardComponent(post) {
             <h1 id="post_date">{post.post_date}</h1>
             <h1 id="post_title">{post.post_title}</h1>
             <p id="post_data">{post.post_data}</p>
-            <div>
-                {[...Array(Number(post.post_rating))].map((el, i) => {
-                    return (
-                        <StarFill key={i} />
-                    )
-                })}
+
+            <div className="POST_CARD_COMPONENT_userName_userrating">
+                <div id="post_rating">
+                    {[...Array(Number(post.post_rating))].map((el, i) => {
+                        return (
+                            <StarFill key={i} />
+                        )
+                    })}
+                </div>
+                <div className="post_user">
+                    <PersonSquare id="post_userProfileImage" />
+                    <h2 id="post_userID">User: {post.post_userId}</h2>
+                </div>
             </div>
 
-            <div className="post_user">
-                <PersonSquare id="post_userProfileImage" />
-                <h2 id="post_userID">User: {post.post_userId}</h2>
-            </div>
         </div>
     )
 }
