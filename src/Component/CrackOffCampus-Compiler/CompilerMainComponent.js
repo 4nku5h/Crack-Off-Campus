35dc6795f4f2161940cb1a6c19274cd17acf0e54
4nku5h <ankush9044045461@gmail.com>
2022-01-06 20:21:17 +0530

    PWA -temp

--- src/Component/CrackOffCampus-Compiler/CompilerMainComponent.js
+++ src/Component/CrackOffCampus-Compiler/CompilerMainComponent.js
@@ -3,15 +3,13 @@ import image_career from "./../../assets/compiler.svg";
 import "./styles.css";
 
 function CompilerMainComponent() {
-    
-    
-
     return (
         <div className="CompilerComponent">
-            <img src={image_career} className="JobsComponent_careerImage_svg" alt="Loading" style={{width:"50%"}}></img>  
-                <h1>Crack Off Campus IDE (Coming Soon)</h1>
+            <h1>Crack Off Campus IDE (Coming Soon)</h1>
+            <img src={image_career} className="JobsComponent_careerImage_svg" alt="Loading" style={{ width: "50%" }}></img>
 
         </div>
     )
 }
+
 export default CompilerMainComponent;
